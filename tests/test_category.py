f2beb01721244c9be8697971c7148b5380a52a3d
Sergey Mosykin <smosyakin@yandex.ru>
2025-12-03 00:52:50 +0300

    add test_product.py, test_category.py

---
+++ tests/test_category.py
@@ -0,0 +1,11 @@
+def test_category_init(first_category, second_category):
+    assert first_category.name == "Смартфоны"
+    assert first_category.description == "Смартфоны, как средство не только коммуникации"
+    assert len(first_category.products) == 3
+    assert second_category.name == "Телевизоры"
+
+    assert first_category.category_count == 2
+    assert second_category.category_count == 2
+
+    assert first_category.product_count == 4
+    assert second_category.product_count == 4
