cc54c3b85667a54c272093f4e336bda3d9ecba5e
Sergey Mosykin <smosyakin@yandex.ru>
2025-12-04 22:11:39 +0300

    fixing category.py

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     """Класс категории продуктов"""
 
@@ -11,6 +14,20 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
-        Category.product_count += len(products)
+        Category.product_count += len(self.__products)
+
+
+    def add_product(self, product: Product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+
+    @property
+    def products(self):
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product.name}, {product.price} Остаток: {product.quantity}шт.\n"
+        return product_str
+
