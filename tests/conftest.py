a2e3a1ec94846d6a972e1df7496fa70f4dc97089
Sergey Mosykin <smosyakin@yandex.ru>
2025-12-05 11:37:44 +0300

    fixing conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,8 @@ def first_category():
         name="Смартфоны",
         description="Смартфоны, как средство не только коммуникации",
         products=[
-            Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
+            Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера",
+                    180000.0, 5),
             Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
             Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14),
         ],
@@ -28,4 +29,6 @@ def second_category():
 
 @pytest.fixture
 def product():
-    return Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 7)
+    return Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера",
+                   180000.0, 7)
+
