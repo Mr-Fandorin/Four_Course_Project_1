89521d67b21c1a0d550e52dddfe0e18116c9c0e4
Sergey Mosykin <smosyakin@yandex.ru>
2025-12-05 11:37:23 +0300

    fixing test_category.py, test_product.py

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,7 +1,7 @@
 def test_category_init(first_category, second_category):
     assert first_category.name == "Смартфоны"
     assert first_category.description == "Смартфоны, как средство не только коммуникации"
-    assert len(first_category.products) == 3
+    assert len(first_category.products_list) == 3
     assert second_category.name == "Телевизоры"
 
     assert first_category.category_count == 2
@@ -9,3 +9,14 @@ def test_category_init(first_category, second_category):
 
     assert first_category.product_count == 4
     assert second_category.product_count == 4
+
+def test_category_products_property(first_category):
+    assert first_category.products == ("Samsung Galaxy C23 Ultra, 180000.0 Остаток: 5шт.\n"
+                                            "Iphone 15, 210000.0 Остаток: 8шт.\n"
+                                            "Xiaomi Redmi Note 11, 31000.0 Остаток: 14шт.\n")
+
+def test_add_product_setter(first_category, product):
+    assert len(first_category.products_list) == 3
+    first_category.add_product(product)
+    assert len(first_category.products_list) == 4
+
