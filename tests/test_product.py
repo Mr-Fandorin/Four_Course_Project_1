b358401f9e7a55e84416db8f07778660a97af1e5
Sergey Mosykin <smosyakin@yandex.ru>
2025-12-05 11:37:28 +0300

    fixing test_category.py, test_product.py

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,20 @@
+from src.product import Product
+
+
 def test_product_init(product):
     assert product.name == "Samsung Galaxy C23 Ultra"
     assert product.description == "256GB, Серый цвет, 200MP камера"
     assert product.price == 180000.0
     assert product.quantity == 7
+
+def test_new_product():
+    product = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера",
+                       180000.0,  5)
+    product.name = "Samsung Galaxy S23 Ultra"
+    product.description = "256GB, Серый цвет, 200MP камера"
+    product.price = 180000.0
+    product.quantity = 5
+
+def test_price(product):
+    product.price = -100
+    assert product.price == 180000.0
