d78225a772e32580d3670c9eedd2b255b95be269
Sergey Mosykin <smosyakin@yandex.ru>
2025-12-09 23:47:28 +0300

    fixing category.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,3 +29,7 @@ def second_category():
 @pytest.fixture
 def product():
     return Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 7)
+
+@pytest.fixture
+def product_2():
+    return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
