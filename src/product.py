1d32478f92b21bf561790b914f3c9659f0b5c679
Sergey Mosykin <smosyakin@yandex.ru>
2025-12-09 23:47:41 +0300

    fixing product.py

--- src/product.py
+++ src/product.py
@@ -12,6 +12,13 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        return f"{self.name}, {self.__price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        return self.price*self.quantity + other.price*other.quantity
+
+
     @classmethod
     def new_product(cls, new_product_dict):
         new_product = cls(**new_product_dict)
