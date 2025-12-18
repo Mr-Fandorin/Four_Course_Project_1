ee1bae3bf26d1534acbb9cfb804f7d8b66c63c76
Sergey Mosykin <smosyakin@yandex.ru>
2025-12-18 22:40:07 +0300

    fixing test_smartphone_product.py

---
+++ tests/test_smartphone_product.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+def test_smartphone_product_init(smartphone_product):
+    assert smartphone_product.name == "Samsung Galaxy S23 Ultra"
+    assert smartphone_product.description == "256GB, Серый цвет, 200MP камера"
+    assert smartphone_product.price == 180000.0
+    assert smartphone_product.quantity == 5
+    assert smartphone_product.efficiency == 95.5
+    assert smartphone_product.model == "S23 Ultra"
+    assert smartphone_product.memory == 256
+    assert smartphone_product.color == "Серый"
+
+def test_smartphone_product_add(smartphone_product, smartphone_product_2):
+    assert smartphone_product + smartphone_product_2 == 2580000
+
+def test_smartphone_product_error(smartphone_product):
+    with pytest.raises(TypeError):
+        result = smartphone_product + 1
